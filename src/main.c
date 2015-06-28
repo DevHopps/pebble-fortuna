9dc14a68a9af0d4a7fd7bdb0f23f9f4f27327379
Benedict Balzer <mail@hopps.me>
2015-06-28 14:00:40 +0200

    Positioning

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@ static void main_window_load(Window *window) {
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_layer));
 
 	//Time layer
-	s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+	s_time_layer = text_layer_create(GRect(0, -5, 144, 50));
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_text_color(s_time_layer, GColorBlack);
 	text_layer_set_text(s_time_layer, "00:00");
