498c209355c670f794b23bc47784eba3880f5b62
Benedict Balzer <mail@hopps.me>
2015-06-28 14:06:37 +0200

    Test

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ static Window *s_main_window;
 static GBitmap *s_bitmap;
 static BitmapLayer *s_bitmap_layer;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 
 static void update_time() {
 	time_t temp = time(NULL); 
@@ -47,10 +48,21 @@ static void main_window_load(Window *window) {
 	text_layer_set_text_color(s_time_layer, GColorBlack);
 	text_layer_set_text(s_time_layer, "00:00");
 
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_BOLD_NUMBERS));
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+	//Date layer
+	// s_date_layer = text_layer_create(GRect(0, 100, 144, 50));
+	// text_layer_set_background_color(s_time_layer, GColorClear);
+	// text_layer_set_text_color(s_time_layer, GColorBlack);
+	// text_layer_set_text(s_time_layer, "28. Jun 2015");
+
+	// text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+	// text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+	// layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
